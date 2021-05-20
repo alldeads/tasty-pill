3f07c24b1f116241e61a26db81ad4c1a5be6b9eb
John Rexter Flores <johnrexter.flores@gmail.com>
2021-05-21 01:06:51 +0800

    Refactor opacity

--- index.php
+++ index.php
@@ -274,7 +274,7 @@
 
         myDropzone.on("processing", function(file) {
             document.getElementById("submit").disabled = true;
-            document.getElementById("submit").style.opacity = 0;
+            document.getElementById("submit").style.opacity = 1;
             document.getElementById("submit").innerHTML = "Processing...";
 
             processing = true;
